97f9a74d2dcbf340f8d5ff4c9cc69f075916839c
Elizabeth Cano <ely.cano2033@gmail.com>
2022-02-20 19:26:53 -0500

    first commit

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-// Require Dependencies
+// Dependencies
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const apiRoutes = require("./Develop/routes/routes");
 
-// Initialize express app
+// Point Server to the route files
+const apiRoutes = require("./Develop/routes/apiRoutes.js");
+const htmlRoutes = require("./Develop/routes/htmlRoutes.js");
+
+// Create an express server
 const app = express();
+
+// Set PORT
 const PORT = process.env.PORT || 3001;
 
-// Setup data parsing
+// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
+// Sets up the static files
+app.use(express.static("public"));
+
+// Parse incoming JSON data
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
-});
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
-});
-//Require routes file
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// Setup listener
+// Listener
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server is ready on port ${PORT}!`);
 });
